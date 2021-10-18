1c5d7bc6c44797d606b4cb461c6617807865fffe
Kurtis <gears2525@aol.com>
2021-10-17 19:59:16 -0700

    Quiz working, added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,79 +3,6 @@ let shuffledQuestions = null;
 let currentQuestion = 0;
 let timePenalty = 10;
 let isGameOver = false;
-
-//Start Game
-$('#start-btn').on('click', function () {
-    countdown();
-    console.log('Started Game!');
-    $('#start-btn').toggleClass('hide');
-    shuffledQuestions = questions.sort(() => Math.random() - .5);
-    console.log(currentQuestion);
-
-    $('#questions').toggleClass('hide');
-    setNextQuestion();
-})
-
-//Selecting Answer
-$('#answer-buttons').on('click', '.btn', function () {
-    console.log(this);
-    if ($(this).hasClass('true')) {
-        $('#result').empty()
-        console.log('correct');
-        $('#result').html('Correct!').addClass('correct');
-    } else {
-        $('#result').empty()
-        console.log('false');
-        timeLeft = timeLeft - timePenalty;
-        $('#result').html(`Wrong! -${timePenalty} seconds`).addClass('wrong');
-    };
-    setNextQuestion();
-});
-
-function setNextQuestion() {
-    if (!isGameOver) {
-        console.log(`Question ${currentQuestion}`);
-        $('#answer-buttons').empty();
-        showQuestion(questions[currentQuestion]);
-        currentQuestion++;
-    } else {
-
-    };
-};
-
-function showQuestion(question) {
-    $('#question').html(question.question);
-    question.answers.forEach(answer => {
-        let button = $('<button>').html(answer.text).addClass('btn').addClass(`${answer.correct}`);
-        $('#answer-buttons').append(button);
-    });
-};
-
-function countdown() {
-    let timeInterval = setInterval(function () {
-        if (timeLeft > 1) {
-            $(".timer").html(timeLeft + ' seconds remaining');
-            timeLeft--;
-        } else if (timeLeft === 1) {
-            $(".timer").html(timeLeft + ' seconds remaining');
-            timeLeft--;
-        } else {
-            clearInterval(timeInterval);
-            gameOver();
-        };
-    }, 1000);
-};
-
-function gameOver() {
-    $('#questions').toggleClass('hide');
-    $(".timer").toggleClass('hide');
-    console.log('GameOver');
-    $('#result').addClass('hide');
-
-};
-
-
-
 const questions = [{
         question: 'Is JavaScript a case-sensitive language?',
         answers: [{
@@ -241,84 +168,201 @@ const questions = [{
         ]
     },
     {
-        question: 'x',
+        question: 'If you type the following code in the console window, what result will you get? 3 > 2 > 1 === false;',
         answers: [{
-                text: '4',
+                text: 'true',
                 correct: true
             },
             {
-                text: '22',
-                correct: false
-            },
-            {
-                text: '22',
-                correct: false
-            },
-            {
-                text: '22',
+                text: 'false',
                 correct: false
             }
         ]
     },
     {
-        question: 'x',
+        question: 'Which of the following will write the message “Hello DataFlair!” in an alert box?',
         answers: [{
-                text: '4',
+                text: 'alertBox(“Hello DataFlair!”);',
+                correct: false
+            },
+            {
+                text: 'alert(Hello DataFlair!);',
+                correct: false
+            },
+            {
+                text: 'msgAlert(“Hello DataFlair!”);',
+                correct: false
+            },
+            {
+                text: 'alert(“Hello DataFlair!”);',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'How do you find the minimum of x and y using JavaScript?',
+        answers: [{
+                text: 'min(x,y);',
+                correct: false
+            },
+            {
+                text: 'Math.min(x,y)',
                 correct: true
             },
             {
-                text: '22',
+                text: 'Math.min(xy)',
                 correct: false
             },
             {
-                text: '22',
-                correct: false
-            },
-            {
-                text: '22',
+                text: 'min(xy);',
                 correct: false
             }
         ]
     },
     {
-        question: 'x',
+        question: 'If the value of x is 40, then what is the output of the following program? (x % 10 == 0)? console.log(“Divisible by 10”) : console.log(“Not divisible by 10”);',
         answers: [{
-                text: '4',
+                text: 'ReferenceError',
+                correct: false
+            },
+            {
+                text: 'Divisible by 10',
                 correct: true
             },
             {
-                text: '22',
+                text: ' Not divisible by 10',
                 correct: false
             },
             {
-                text: '22',
-                correct: false
-            },
-            {
-                text: '22',
+                text: 'None of the above',
                 correct: false
             }
         ]
     },
     {
-        question: 'x',
+        question: 'Which JavaScript label catches all the values, except for the ones specified?',
         answers: [{
-                text: '4',
+                text: 'catch',
+                correct: false
+            },
+            {
+                text: 'label',
+                correct: false
+            },
+            {
+                text: 'try',
+                correct: false
+            },
+            {
+                text: 'default',
+                correct: true
+            }
+        ]
+    },
+    {
+        question: 'Which are the correct “if” statements to execute certain code if “x” is equal to 2?',
+        answers: [{
+                text: ' if(x 2)',
+                correct: false
+            },
+            {
+                text: 'if(x = 2)',
+                correct: false
+            },
+            {
+                text: 'if(x == 2)',
                 correct: true
             },
             {
-                text: '22',
-                correct: false
-            },
-            {
-                text: '22',
-                correct: false
-            },
-            {
-                text: '22',
+                text: 'if(x != 2 )',
                 correct: false
             }
         ]
     }
 
-] //work in progres
+]
+
+function setNextQuestion() {
+    if (!isGameOver) {
+        console.log(`Question ${currentQuestion}`);
+        $('#answer-buttons').empty();
+        showQuestion(questions[currentQuestion]);
+        currentQuestion++;
+    } else {
+
+    };
+};
+
+function showQuestion(question) {
+    $('#question').html(question.question);
+    question.answers.forEach(answer => {
+        let button = $('<button>').html(answer.text).addClass('btn').addClass(`${answer.correct}`);
+        $('#answer-buttons').append(button);
+    });
+};
+
+function countdown() {
+    let timeInterval = setInterval(function () {
+        if (timeLeft > 1) {
+            $("#timer").html(timeLeft + ' seconds remaining');
+            timeLeft--;
+        } else if (timeLeft === 1) {
+            $("#timer").html(timeLeft + ' seconds remaining');
+            timeLeft--;
+        } else {
+            clearInterval(timeInterval);
+            gameOver();
+        };
+    }, 1000);
+};
+
+function gameOver() {
+    $('#questions').toggleClass('hide');
+    $("#timer").toggleClass('hide');
+    console.log('GameOver');
+    $('#result').toggleClass('hide');
+    $('#high-score').toggleClass('hide');
+    $('#result').html('');
+};
+
+$('#start-btn').on('click', function () {
+    countdown();
+    console.log('Started Game!');
+    $('.controls').toggleClass('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    console.log(currentQuestion);
+    $('#questions').toggleClass('hide');
+    $('#timer').toggleClass('hide');
+    $('#high-score-btn').toggleClass('hide')
+    setNextQuestion();
+});
+
+//Selecting Answer
+$('#answer-buttons').on('click', '.btn', function () {
+    console.log(this);
+    if ($(this).hasClass('true')) {
+        console.log('correct');
+        $('#result').html('Correct!');
+    } else {
+        console.log('false');
+        timeLeft = timeLeft - timePenalty;
+        $('#result').html(`Wrong! -${timePenalty} seconds`);
+    };
+    setNextQuestion();
+});
+
+$('#high-score-btn').on('click', function () {
+    $('.controls').toggleClass('hide');
+    $('#high-score').toggleClass('hide');
+    $('#score-list').toggleClass('hide');
+});
+
+$('#submit-btn').on('click', function () {
+    localStorage.setItem(JSON.stringify($('#score').val()), JSON.stringify(timeLeft));
+    location.reload();
+});
+
+for (var i = 0, len = localStorage.length; i < len; ++i) {
+    let tempData = localStorage.key(i) + localStorage.getItem(localStorage.key(i))
+    $('#score-list').append(`<li>${tempData}</li>`);
+};
